45e3bc0fbc88739ead2f5b3e307628815711ce0e
Subha Ghanta <subhaghanta60@gmail.com>
2024-08-21 12:13:14 +0530

    feat:Radio Chart Design

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,10 +1,11 @@
+import CountChart from "@/components/CountChart"
 import UserCard from "@/components/UserCard"
 
 const AdminPage = () => {
   return (
     <div className='p-4 flex gap-4 flex-col md:flex-row'>
       {/* Left Side*/}
-        <div className="w-full lg:w-2/3">
+        <div className="w-full lg:w-2/3 flex flex-col gap-8">
           {/* User Card */} 
           <div className="flex gap-4 justify-between flex-wrap">
             <UserCard type="Student"/>
@@ -12,12 +13,28 @@ const AdminPage = () => {
             <UserCard type="Parent"/>
             <UserCard type="Stuff"/>
           </div>
+          {/* Middle Chart */}
+          <div className="flex gap-4 flex-col lg:flex-row">
+            {/*Count Chart */}
+            <div className="w-full lg:w-1/3 h-[450px]">
+              <CountChart />
+            </div>
+            {/* Attendence Chart */}
+            <div className="w-full lg:w-2/3 h-[450px]">
+
+            </div>
+            
+          </div>
+          {/* Bottom Chart */}
+          <div></div>
         </div>
+        
       
 
       {/* Right Side */}
         <div className="w-full lg:w-1/3">r</div>
-        </div>
+
+    </div>
   )
 }
 
