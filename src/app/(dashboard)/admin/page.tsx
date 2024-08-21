3bd953d843ef37c63f838a5acfaca14ca2fdf0d9
Subha Ghanta <subhaghanta60@gmail.com>
2024-08-21 10:43:06 +0530

    User Card Component Design

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,23 @@
+import UserCard from "@/components/UserCard"
+
 const AdminPage = () => {
   return (
-    <div className=''>AdminPage</div>
+    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+      {/* Left Side*/}
+        <div className="w-full lg:w-2/3">
+          {/* User Card */} 
+          <div className="flex gap-4 justify-between flex-wrap">
+            <UserCard type="Student"/>
+            <UserCard type="Teacher"/>
+            <UserCard type="Parent"/>
+            <UserCard type="Stuff"/>
+          </div>
+        </div>
+      
+
+      {/* Right Side */}
+        <div className="w-full lg:w-1/3">r</div>
+        </div>
   )
 }
 
